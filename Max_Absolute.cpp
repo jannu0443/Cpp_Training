4f00c0fbe2185070b097d9a4e083f1d8065a3f03
jannu0443 <141567203+jannu0443@users.noreply.github.com>
2024-10-21 12:26:35 +0530

    Create Max_Absolute.cpp

---
+++ Max_Absolute.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+#define MAX(a,b) ((a)>(b)?(a):(b))
+using namespace std;
+
+int main()
+{
+std::cout <<"Max Number is " << MAX(10,2) << endl;
+std::cout << "Absolute value is " << abs(-10.2125) << endl;
+
+return 0;
+}
